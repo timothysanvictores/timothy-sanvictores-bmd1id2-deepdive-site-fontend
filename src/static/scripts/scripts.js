e1f5a33a63f171fba8b73609982873d0e1f72a7a
timothysanvictores <timothy.sanvictores@mds.ac.nz>
2026-04-12 22:15:35 +1200

    developed responsive homepage and navigation system

---
+++ src/static/scripts/scripts.js
@@ -0,0 +1,30 @@
+function mobileNavMenu() {
+	const hamburgerIcon = document.getElementById("hamburger-icon-large");
+	const closeIcon = document.getElementById("close-icon-large");
+	const mobileNav = document.getElementById("mobile-nav");
+	// const main = document.getElementsByTagName("main").getElementByTagName("main");
+	// const footer = document.getElementsByTagName("footer").getElementByTagName("main");
+
+	const main = document.getElementsByTagName("main")[0];
+	const footer = document.getElementsByTagName("footer")[0];
+
+	main.style.display = "none";
+	footer.style.display = "none";
+	hamburgerIcon.style.display = "none";
+	closeIcon.style.display = "block";
+	mobileNav.style.display = "flex";
+}
+
+function closeMobileNavMenu() {
+	const hamburgerIcon = document.getElementById("hamburger-icon-large");
+	const closeIcon = document.getElementById("close-icon-large");
+	const mobileNav = document.getElementById("mobile-nav");
+	const main = document.getElementsByTagName("main")[0];
+	const footer = document.getElementsByTagName("footer")[0];
+
+	main.style.display = "flex";
+	footer.style.display = "block";
+	hamburgerIcon.style.display = "block";
+	closeIcon.style.display = "none";
+	mobileNav.style.display = "none";
+}
